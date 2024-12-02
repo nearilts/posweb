891d979f82d684c13de2cf102aaf5ed2b14a55f4
adifahram <adifahram23@gmail.com>
2024-12-02 10:48:34 +0700

    modal edit berdasarkan id

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -78,8 +78,7 @@
                           <a target="_blank" href="{{route('transaction.show',$d)}}" class="btn btn-success btn-sm"><i class="fa fa-file"></i></a>
 
                         </div>
-
-                        <div class="modal fade" id="edit_pembelian_<?php echo $d['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="edit_pembelian_{{ $d['id'] }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                           <div class="modal-dialog" role="document">
                             <div class="modal-content">
                               <div class="modal-header">
@@ -93,81 +92,37 @@
                                 @method('PUT')
                                 <div class="modal-body">
                                   <div class="row">
-                        
                                     <div class="col-lg-12">
-                                     <label>Kasir yang melayani</label>
-                                     <br>
-                                     <?php echo $d['kasir']; ?>
-                                   </div>
-                        
-                                   <br>
-                                   <br>
-                                   <br>
-                        
-                                   <div class="col-lg-4">
-                        
-                                    <div class="form-group">
-                                      <label>No. Invoice</label>
-                                      <br>
-                                      <?php echo $d['invoice_no']; ?>
+                                      <label>Kasir yang melayani</label>
+                                      <p>{{ $d['kasir'] }}</p>
                                     </div>
-                        
-                                  </div>
-                                  <div class="col-lg-4">
-                        
-                                    <div class="form-group">
-                                      <label>Tanggal Invoice</label>
-                                      <br>
-                                      <?php echo date('d-m-Y', strtotime($d['tanggal'])); ?>
-                                    </div>
-                        
-                                  </div>
-                                  <div class="col-lg-4">
-                        
-                                    <div class="form-group">
-                                      <label>Pelanggan</label>
-                                      <br>
-                                      <?php echo $d['nama_pelanggan']; ?>
-                                    </div>
-                        
-                                  </div>
-                        
                                     <div class="col-lg-4">
-                        
-                                    <div class="form-group">
+                                      <label>No. Invoice</label>
+                                      <p>{{ $d['invoice_no'] }}</p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                      <label>Tanggal Invoice</label>
+                                      <p>{{ date('d-m-Y', strtotime($d['tanggal'])) }}</p>
+                                    </div>
+                                    <div class="col-lg-4">
+                                      <label>Pelanggan</label>
+                                      <p>{{ $d['nama_pelanggan'] }}</p>
+                                    </div>
+                                    <div class="col-lg-4">
                                       <label>Alamat</label>
-                                      <br>
-                                      <?php echo $d['alamat']; ?>
+                                      <p>{{ $d['alamat'] }}</p>
                                     </div>
-                        
-                                  </div>
-                        
-                                  <div class="col-lg-4">
-                        
-                                    <div class="form-group">
+                                    <div class="col-lg-4">
                                       <label>Tlp</label>
-                                      <br>
-                                      <?php echo $d['telp']; ?>
+                                      <p>{{ $d['telp'] }}</p>
                                     </div>
-                        
-                                  </div>
-                        
-                                  <div class="col-lg-4">
-                        
-                                    <div class="form-group">
+                                    <div class="col-lg-4">
                                       <label>Tanggal Pemasangan</label>
-                                      <br>
-                                      <?php echo date('d-m-Y', strtotime($d['tanggal_kirim'])); ?>
+                                      <p>{{ date('d-m-Y', strtotime($d['tanggal_kirim'])) }}</p>
                                     </div>
-                        
                                   </div>
-                        
-                                </div>
-                        
-                                <hr>  
-                        
-                                <b>Daftar Pembelian</b>
-                        
+                                  <hr>
+                                  <b>Daftar Pembelian</b>
                                   <div class="table-responsive">
                                     <table class="table table-bordered table-hover">
                                       <thead>
@@ -179,20 +134,20 @@
                                         </tr>
                                       </thead>
                                       <tbody>
-                                        <?php foreach($d->detail as $ky => $pp) { ?>
+                                        @foreach($d->detail as $ky => $pp)
                                           <tr>
                                             <td>{{ $pp['product'] }}</td>
                                             <td>Rp.{{ number_format($pp['price']) }}</td>
                                             <td>
-                                              <input type="hidden" name="detail[{{ $ky }}][id]" value="{{ $pp['id'] }}" class="form-control " >
-                                              <input type="hidden" name="detail[{{ $ky }}][product_id]" value="{{ $pp['product_id'] }}" class="form-control " >
-                                              <input type="number" name="detail[{{ $ky }}][qty]" value="{{ $pp['qty'] }}" class="form-control qty-input" data-price="{{ $pp['price'] }}">
+                                              <input type="hidden" name="detail[{{ $ky }}][id]" value="{{ $pp['id'] }}">
+                                              <input type="hidden" name="detail[{{ $ky }}][product_id]" value="{{ $pp['product_id'] }}">
+                                              <input type="number" name="detail[{{ $ky }}][qty]" id="qty-{{ $d['id'] }}-{{ $ky }}" value="{{ $pp['qty'] }}" class="form-control qty-input-{{ $d['id'] }}" data-price="{{ $pp['price'] }}" min="1">
                                             </td>
                                             <td>
-                                              <span class="item-total">Rp.{{ number_format($pp['qty'] * $pp['price']) }}</span>
+                                              <span id="item-total-{{ $d['id'] }}-{{ $ky }}">Rp.{{ number_format($pp['qty'] * $pp['price']) }}</span>
                                             </td>
                                           </tr>
-                                        <?php } ?>
+                                        @endforeach
                                       </tbody>
                                     </table>
                                   </div>
@@ -202,38 +157,66 @@
                                         <tr>
                                           <th>Sub Total</th>
                                           <td>
-                                            <input type="hidden" name="sub_total" value="{{ $d['sub_total'] }}" class="form-control sub_total" >
-                                            <span id="edit-sub-total">Rp.{{ number_format($d['sub_total']) }}</span></td>
+                                            <span id="edit-sub-total-{{ $d['id'] }}">Rp.{{ number_format($d['sub_total']) }}</span>
+                                            <input type="hidden" name="sub_total" value="{{ $d['sub_total'] }}" class="sub-total-{{ $d['id'] }}">
+                                          </td>
                                         </tr>
                                         <tr>
                                           <th>Total Paid</th>
                                           <td>
-                                            <input type="number" name="paid" id="paid" value="{{ $d['paid'] }}" class="form-control paid-input">
+                                            <input type="number" name="paid" id="paid-{{ $d['id'] }}" value="{{ $d['paid'] }}" class="form-control paid-input-{{ $d['id'] }}" min="0">
                                           </td>
                                         </tr>
                                         <tr>
                                           <th>Unpaid</th>
                                           <td>
-                                            <input type="number" name="unpaid" id="unpaid" value="{{ $d['unpaid'] }}" class="form-control unpaid-input">
+                                            <input type="number" name="unpaid" id="unpaid-{{ $d['id'] }}" value="{{ $d['unpaid'] }}" class="form-control unpaid-input-{{ $d['id'] }}" readonly>
                                           </td>
                                         </tr>
                                         <tr>
                                           <th>Total</th>
                                           <td>
-                                            <input type="hidden" name="total" value="{{ $d['total'] }}" class="form-control totals" >
-                                            <span id="edit-total">Rp.{{ number_format($d['total']) }}</span></td>
+                                            <span id="edit-total-{{ $d['id'] }}">Rp.{{ number_format($d['total']) }}</span>
+                                            <input type="hidden" name="total" value="{{ $d['total'] }}" class="totals-{{ $d['id'] }}">
+                                          </td>
                                         </tr>
                                       </table>
                                     </div>
-                                </div>
-                                <div class="modal-footer">
-                                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-                                  <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
-                                </div>
+                                  </div>
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                    <button type="submit" class="btn btn-primary">Simpan Perubahan</button>
+                                  </div>
                               </form>
                             </div>
                           </div>
                         </div>
+                        
+                        @push('script')
+                        <script>
+                          $(document).on('input', '.qty-input-{{ $d['id'] }}, .paid-input-{{ $d['id'] }}', function() {
+                            let subTotal = 0;
+                        
+                            $('.qty-input-{{ $d['id'] }}').each(function() {
+                              let qty = $(this).val() || 0;
+                              let price = $(this).data('price');
+                              subTotal += price * qty;
+                        
+                              let rowId = $(this).attr('id').split('-').pop();
+                              $('#item-total-{{ $d['id'] }}-' + rowId).text('Rp.' + (price * qty).toLocaleString());
+                            });
+                        
+                            $('#edit-sub-total-{{ $d['id'] }}').text('Rp.' + subTotal.toLocaleString());
+                            $('.sub-total-{{ $d['id'] }}').val(subTotal);
+                        
+                            let paid = parseFloat($('#paid-{{ $d['id'] }}').val()) || 0;
+                            let unpaid = subTotal - paid;
+                        
+                            $('#unpaid-{{ $d['id'] }}').val(unpaid);
+                            $('#edit-total-{{ $d['id'] }}').text('Rp.' + subTotal.toLocaleString());
+                          });
+                        </script>
+                        @endpush
 
                         
                         <div class="modal fade" id="detail_pembelian_<?php echo $d['id'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -422,35 +405,3 @@
 
 @endsection
 
-
-
-@push('script')
-<script>
-  $(document).on('input', '.qty-input, .paid-input, .unpaid-input', function() {
-    let subTotal = 0;
-
-    $('.qty-input').each(function() {
-      let qty = $(this).val();
-      let price = $(this).data('price');
-      subTotal += price * qty;
-    });
-
-    $('.item-total').text('Rp.' + subTotal.toLocaleString());
-    $('#edit-sub-total').text('Rp.' + subTotal.toLocaleString());
-    $('.sub_total').val(subTotal);
-    $('.totals').val(subTotal);
-
-    let discount = {{ $d['diskon'] }};
-    let finalTotal = subTotal - (subTotal * discount / 100);
-
-    $('#edit-total').text('Rp.' + finalTotal.toLocaleString());
-
-    let sisa = finalTotal - $('.paid-input').val() ;
-
-    $('.unpaid-input').val(sisa);
-
-
-
-  });
-</script>
-@endpush
